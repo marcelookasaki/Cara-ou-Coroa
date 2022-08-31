b712fad42067f2ce4372b80af8e5348253ef4453
marcelookasaki <marcelo.okasaki@gmail.com>
2022-08-31 09:46:19 -0300

    Initial Commit

---
+++ Cara ou Coroa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cara ou Coroa
+//
+//  Created by Marcelo on 8/31/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
